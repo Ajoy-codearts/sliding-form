7dbb418167022aaf7342fe2f097e34a88f3fd2c5
unknown <ajay.codearts@gmail.com>
2022-03-04 10:45:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let main_form = document.getElementById('main_form');
+
+function signup(){
+    main_form.classList.add('shift');
+}
+function signin(){
+    main_form.classList.remove('shift');
+}
